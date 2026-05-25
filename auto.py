1bb417209465d3d5f6538fd99ae644826dcb8a5f
agusherrera99 <agustinherrera.dev@gmail.com>
2026-05-25 18:10:19 -0300

    update(auto): añade el método 'guardar_auto' para insertar un nuevo regustro en la tabla 'autos' y 'seleccionar_todos' para poder obtener todos los registros de la tabla 'autos'. se añadio la variable de clase database para poder ejecutar queries en los métodos disponibles

--- auto.py
+++ auto.py
@@ -1,6 +1,23 @@
+from database import Database
+
+
 class Auto:
     def __init__(self):
-        pass
+        self.database = Database()
 
-    def nuevo(self, datos):
-        pass
+    def guardar_auto(self, datos):
+        query = """
+        INSERT INTO autos (patente, marca, modelo, anio, kilometros, precio, estado, fecha_ingreso)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+        """
+        self.database.ejecutar_query(query, datos)
+        print("Auto guardado correctamente")
+
+    def seleccionar_todos(self):
+        query = """
+        SELECT * FROM autos
+        """
+
+        respuesta = self.database.ejecutar_query(query)
+        resultado = respuesta.fetchall()
+        return resultado
